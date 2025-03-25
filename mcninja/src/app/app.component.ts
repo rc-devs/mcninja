57a8db9d58567a426f367a065e62474e268cf599
rc-devs <rc.devs9@gmail.com>
2025-03-25 16:08:15 -0500

    connect display techniques to app, change data from variable to property in DisplayTechniques. confirm display in app

--- mcninja/src/app/app.component.ts
+++ mcninja/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DisplayTechniquesComponent } from './display-techniques/display-techniques.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [DisplayTechniquesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
