d7ab7322f8da7014c702b563440b9af84179b8cd
rc-devs <rc.devs9@gmail.com>
2025-03-26 17:36:03 -0500

    add button to display-techniques html and  connect button to display techniques ts

--- mcninja/src/app/display-techniques/display-techniques.component.ts
+++ mcninja/src/app/display-techniques/display-techniques.component.ts
@@ -1,16 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { Technique } from '../data/techinque.template';
 import techniquesArray from '../data/techniques.array';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-display-techniques',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './display-techniques.component.html',
   styleUrl: './display-techniques.component.css',
   standalone: true,
 })
 export class DisplayTechniquesComponent {
  techniques = signal<Technique[]>(techniquesArray)
+
+ toggleMastered(){
+  console.log('tiggle toggle')
+ }
 }
 
