7d5501debaf2f130e90a131fcc5989a4e00775ea
rc-devs <rc.devs9@gmail.com>
2025-03-26 13:20:39 -0500

    data entry; basic tan unarmed techniques

--- mcninja/src/app/data/techniques.array.ts
+++ mcninja/src/app/data/techniques.array.ts
@@ -1,9 +1,166 @@
 //default export at very bottom of array
+//using default export and assigning as variable makes the template useless...
 //need to import DomSanitizer for youtube embeds? URL issue?
 
 let techniquesArray = [
     {
-      id: "tan01",
+      id: 't1',
+      title: 'Lead Hand Punch',
+      belt: 'Tan',
+      execution: `
+      The lead hand punch is a snapping straight punch executed by the forward or lead hand. It is a fast, unexpected punch designed to stun an aggressor and to set up for a follow-on techniques. A lead hand punch conceals movement and allows you to get close to the aggressor.
+      (1) Assume the basic warrior stance.
+      (2) Snap your lead hand out to nearly full extension, while rotating your palm to the deck.
+      (3) Keep your rear hand in place to protect your head.
+      (4) Rapidly retract to the basic warrior stance.
+      `,
+      proTip: "A common mistake is to pull the fist back low, leaving you open to a counter attack. Ensure you do not hyperextend your elbows",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8Oxwfhd_L0Q?si=ggknHEo16ij2KbMT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't2',
+      title: 'Rear Hand Punch',
+      belt: 'Tan',
+      execution: `
+      The rear hand punch is a snapping punch executed by the rear hand. It is a power punch designed to inflict maximum damage on your aggressor. Its power comes from pushing off your rear leg and rotating your hips and shoulders.
+      (1) Assume the basic warrior stance.
+      (2) Forcefully rotate your hips and shoulders toward the aggressor and thrust your rear hand straight out, palm down, to nearly full extension.
+      (3) Shift your body weight to your lead foot while pushing off on the ball of your rear foot.
+      (4) Keep your lead hand in place to protect your head.
+      (5) Rapidly retract to the basic warrior stance.
+      `,
+      proTip: "A common mistake is to pull the fist back low, leaving you open to a counter attack. Ensure you do not hyperextend your elbows",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/ofil5E5lPCY?si=vci54z7U6Vb-NFcL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't3',
+      title: 'Uppercut',
+      belt: 'Tan',
+      execution: `
+      The uppercut is a powerful punch originating below the aggressor's line of vision. It is executed in an upward motion traveling up the centerline of the aggressor's body. It is delivered in close and usually follows a preparatory strike that leaves the target area unprotected. When delivered to the chin or jaw, the uppercut can render an aggressor unconscious, cause extensive damage to the neck, and/or sever the tongue.
+      (1) Assume the basic warrior stance.
+      (2) Rotate your fist so your palm is facing you. Ensure your lead hand stays up and in place to protect your head.
+      (3) Power is generated from low to high. Start with your body weight low and legs slightly bent. Explode upwards with your legs, hips, and shoulders, driving your fist straight up through the target area.
+      (4) Rapidly retract to the basic warrior stance
+      `,
+      proTip: "A common mistake is to drop the fist to the waist in an attempt to generate more power. Power is generated from the correct use of the lower body.",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/5LXsw2VfrOg?si=YFUX3n1gZehr2fvi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't4',
+      title: 'Hook',
+      belt: 'Tan',
+      execution: `The hook is a powerful punch that is executed close-in and is usually preceded by a preparatory strike.
+        (1) Assume the basic warrior stance.
+        (2) Rotate your rear fist, this will parallel your fist and forearm to the deck.
+        (3) Power is generated from side to side by driving with your legs and rotating your hips and shoulders. Your body’srotation drives the fist through your target area as your lead hand stays up and in place to protect your head.
+        (4) Rapidly retract to the basic warrior stance.`,
+        proTip: "A common mistake is to reach or extend the fist all the way out in an attempt to generate more power. Power is generated from the rotation of the hips and shoulders.",
+        video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/_nCUoFrqibk?si=CLu3yeW1XYfNr4w9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't5',
+      title: 'Eye Gouge',
+      belt: 'Tan',
+      execution: `The eye gouge is used to attack the aggressor’s eyes, blinding him so follow on techniques can be executed.
+      (1) From the basic warrior stance, open your left hand with your fingers spread apart and slightly bent.
+      (2) With the palm of your hand either toward the ground or toward the sky, thrust your left hand forward into the aggressor's eyes.
+      (3) Thrust your hand at the aggressor's nose level so your fingers can slide naturally into the grooves of their eye sockets.
+      (4) Return to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/AZvW31qC4vc?si=mcnYpG_JZWXsyAjj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't6',
+      title: 'Horizontal Hammer Fist',
+      belt: 'Tan',
+      execution: `Striking with the hammer fist concentrates power in a small part of the hand which can have a devastating effect when transferred to the target.
+        (1) From the basic warrior stance, retract your right hand so your fist is next to your face. Simultaneously rotate your right hip and shoulder backwards.
+        (2) Thrust your fist forward onto the aggressor while rotating your right hip and shoulder forward. Rotate your wrist so the meaty portion of your fist makes contact on the aggressor.
+        (3) Follow through the target area with your fist.
+        (4) Return to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/o8eqizg-IFw?si=7O_gj8u3VaTvM6Mz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't7',
+      title: 'Vertical Hammer Fist',
+      belt: 'Tan',
+      execution: `Striking with the hammer fist concentrates power in a small part of the hand which, when transferred to the target, can have a devastating effect.
+        (1) From the basic warrior stance, retract your right hand so your fist is next to your face. Simultaneously rotate your right hip and shoulder backwards.
+        (2) Thrust your fist straight down onto the aggressor while dropping your body weight. Rotate your wrist so the meaty portion of your fist makes contact on the aggressor.
+        (3) Follow through the target area with your fist.
+        (4) Return to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/iEfiIQC1NQA?si=svXoE3sLbqjm3mI9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't8',
+      title: 'Vertical Elbow Strike, Low to High',
+      belt: 'Tan',
+      execution: ` Elbow strikes are close range weapons that inflict a great amount of damage due to leverage and the transfer of your body weight.
+          (1) From the basic warrior stance, bend your right elbow keeping your fist close to your body. Your fist is at shoulder level and your elbow is next to your torso.
+          (2) Thrust your elbow vertically upward toward the aggressor. Keep your elbow bent throughout the movement.
+          (3) Rotate your right shoulder and hip forward and drive up with your legs to generate power.
+          (4) Make contact on the aggressor with your right forearm two inches above the point of the elbow.
+          (5) Return to the basic warrior stance`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/74pmwIZcn10?si=rSEZrLW1SvRZekJV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't9',
+      title: 'Forward Horizontal Elbow Strike',
+      belt: 'Tan',
+      execution: `Elbow strikes are close range weapons that inflict a great amount of damage due to leverage and the transfer of your body weight.
+          (1) From the basic warrior stance, bring your elbow up and tuck your right fist near your chest with the palm facing the deck.
+          (2) Thrust your right elbow horizontally forward toward the aggressor, keeping your forearm parallel to the deck.
+          (3) Keep your fist tucked near your chest with the palm heel facing the ground, and keep your elbow bent throughout the movement.
+          (4) Rotate your right shoulder and hip forward to generate power.
+          (5) Return to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/xHrDVdlzV08?si=cnxzU9b5YkHGZqi9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't10',
+      title: 'Rear Choke',
+      belt: 'Tan',
+      execution: `The rear choke is performed when you are behind the aggressor, when he is on the ground, or when you are taking him to the ground.
+(1) Begin with the aggressor kneeling on the deck while you stand behind him.
+(2) With your right arm, reach over the aggressor’s right shoulder and hook the bend of your arm around his neck. Your chest should be against the aggressor’s back.
+(3) Clasp both hands together, palm-on-palm, with your right palm facing the deck.
+(4) Ensure the aggressor’s trachea is positioned within the bend of your arm, but pressure is not being exerted on it.
+(5) Exert pressure with your right biceps and radius bone on the aggressor’s carotid arteries, on both sides of the neck.
+(6) Maintain pressure on both sides of the neck and draw the aggressor closer to you by pulling your right arm in.
+(7) To increase the effectiveness of the choke, apply forward pressure to the back of the aggressor's head with your head.
+`,
+      proTip: "If the aggressor is taller than you, step or push on the back of the aggressor's knee with your foot. This will off-balance the aggressor, causing him to bend at the knees and fall forward.",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/LQqpWmQ01SI?si=nwW-DcVAubUvKucw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't11',
+      title: 'Figure 4 Variation',
+      belt: 'Tan',
+      execution: `The figure-four is a variation that allows you to gain more leverage on the rear choke. If you cannot secure the rear choke, you may apply the figure-four variation to increase the pressure of the choke on the aggressor.
+(1) Apply a rear choke. Your body should be against the aggressor's body.
+(2) Bring your left arm over the aggressors left shoulder, grasp your left biceps or shoulder with your right hand, and place your left hand against the back of the aggressor's head.
+(3) Push the aggressor's head forward and down with your left hand, keeping your elbows in.
+(4) Draw your right arm in, maintaining pressure with your biceps and forearm on both sides of the aggressor's neck.`,
+      proTip: "If the aggressor is taller than you, step or push on the back of the aggressor's knee with your foot. This will off-balance the aggressor, causing him to bend at the knees and fall forward.",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/dpHo0zG93mY?si=4GmmJ2zaLUBeEt3s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: "t12",
       title: "Leg Sweep",
       belt: 'Tan',
       execution:
@@ -26,14 +183,59 @@ let techniquesArray = [
       mastered: false,
     },
     {
-      id: '',
+      id: 't13',
       title: 'Front Kick',
       belt: 'Tan',
-      execution: "",
+      execution: `The front kick is used to stop the aggressor’s forward momentum or to set him up for follow on techniques when the aggressor is in front of you.
+(1) From the basic warrior stance, raise your right knee waist high and pivot your hips into the attack, thrusting your right foot forward toward the aggressor. You may have to shift your body weight to your left leg to maintain balance.
+(2) Never extend your foot above waist high, and keep in mind it is difficult to change the direction of a kick after it is initiated.
+(3) Make contact on the aggressor with the toe of your right boot or bootlaces.
+(4) Follow through the target with your foot and rapidly retract to the basic warrior stance.`,
       proTip: "",
       video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/JJdXqRpzp5o?si=ISnypdLCOcAFdi_r" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
       mastered: false,
     },
-
+    {
+      id: 't14',
+      title: 'Round Kick',
+      belt: 'Tan',
+      execution: `The round kick is used to cause maximum damage to the knee or to set up the aggressor for follow on techniques.
+(1) From the basic warrior stance, point the foot of your left leg 45 degrees to the outside of the aggressor and raise your right leg slightly off the deck. You may have to shift your body weight to your left leg to maintain balance.
+(2) With your left leg slightly bent, thrust your right leg forward in an arcing motion towards the aggressor, rotating your hips and shoulders into the attack.
+(3) Make contact on the aggressor with the shin of the right leg or the top of the foot.
+(4) Follow through the target with your foot and rapidly retract to the basic warrior stance.
+`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/R1x5rcb7aUA?si=Dajj3_DSsFHhIrTW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't15',
+      title: 'Vertical Stomp',
+      belt: 'Tan',
+      execution: `The vertical stomp is used to attack the aggressor when he is on the deck and you are standing.
+(1) From the basic warrior stance, raise the knee of your right foot above waist level. Your right leg should be bent at a 90 degree angle. Shift your body weight to your left leg to maintain your balance.
+(2) Forcefully drive the flat bottom of your right boot down onto the aggressor while keeping your right knee slightly bent.
+(3) At the same time, bend your left knee slightly to drop your body weight into the strike.
+(4) Rapidly retract to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/K_JpSishBiI?si=Q95JYAvmgiW1mwSE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
+    {
+      id: 't16',
+      title: 'Vertical Knee Strike',
+      belt: 'Tan',
+      execution: `The vertical knee strike is used to inflict major damage during close range fighting or grappling.
+(1) From the basic warrior stance, grab the aggressor’s neck or gear with both hands without interlacing your fingers. If you interlace your fingers, your aggressor can pull back or reach behind their head and break the fingers.
+(2) Pull the aggressor down and at the same time raise your right knee driving it up forcefully into the aggressor. Power is generated by pulling the aggressor down and thrusting the leg upward with your hips.
+(3) Follow through the target with your knee and rapidly retract to the basic warrior stance.`,
+      proTip: "",
+      video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/ZaRTJZwXorI?si=4cpuIpwKyyG5tqbq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    // <iframe width="560" height="315" src="https://www.youtube.com/embed/DEhzD274t6A?si=TZ_Rk8lP5qt0ESjA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+      mastered: false,
+    },
   ]
-  export default techniquesArray;
+
+
+export default techniquesArray;
