e5519f6b330508d5def2cc291032f56aac5a2a47
rc-devs <rc.devs9@gmail.com>
2025-03-29 12:13:28 -0500

    pseudocode notes

--- mcninja/src/app/display-techniques/display-techniques.component.ts
+++ mcninja/src/app/display-techniques/display-techniques.component.ts
@@ -14,35 +14,14 @@ export class DisplayTechniquesComponent {
   technique = inject(TechniqueService) //technique signal for dynamic use in html
 
 
-
-
-
-
-
 //toggle mastered for dynamic update of html
  toggleMastered(masteredValue:boolean, techniqueId:string){
   //probably just use .find(techniqueId)
 
-  let index = 1 + (this.technique.techniques().reduce((accumulator, current, currentIndex) => {
-    if (current.id === techniqueId) {
-      return currentIndex;
-      }
-      return accumulator;
-    }, -1));
-
-    let strungIndex = index.toString()
-    if (techniqueId == 't'+strungIndex){
-      console.log('match')
-      console.log(techniqueId, 't'+strungIndex)
-    } else {
-      console.log('no match')
-      console.log(techniqueId, 't'+strungIndex)
-    }
-
+   //get object via id
+   //update boolean to opposite of what it currently is
+   //confirm that "proficiency" is update in html
   }
 
-
 }
-
-
 
