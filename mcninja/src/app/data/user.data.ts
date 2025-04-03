1b9eccd992492e1d66e9c34b70581fd2a4e67c0e
rc-devs <rc.devs9@gmail.com>
2025-04-03 15:30:29 -0500

    more dummy users

--- mcninja/src/app/data/user.data.ts
+++ mcninja/src/app/data/user.data.ts
@@ -6,5 +6,21 @@ export default [
     email: 'ya_mum@yafamily.com',
     userMastered: [],
     userNotMastered: [],
-  }
+  },
+  {
+    id: 'u2',
+    nameFirst: 'Ya ',
+    nameLast: 'Dad',
+    email: 'ya_dad@yafamily.com',
+    userMastered: [],
+    userNotMastered: [],
+  },
+  {
+    id: 'u3',
+    nameFirst: 'Ya ',
+    nameLast: 'Dog',
+    email: 'ya_dog@yafamily.com',
+    userMastered: [],
+    userNotMastered: [],
+  },
 ]
