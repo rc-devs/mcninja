0d551b14c892b69d0193a366bbc63cfa38885cc7
rc-devs <rc.devs9@gmail.com>
2025-04-05 19:21:05 -0500

    Technique interface for masteredArrays

--- mcninja/src/app/data/user.data.ts
+++ mcninja/src/app/data/user.data.ts
@@ -1,26 +1,28 @@
+import { Technique } from "./models/techinque.model";
+
 export default [
   {
     id: 'u1',
     nameFirst: 'Ya ',
     nameLast: 'mum',
     email: 'ya_mum@yafamily.com',
-    userMastered: [],
-    userNotMastered: [],
+    userMastered: <Technique[]>[],
+    userNotMastered: <Technique[]>[],
   },
   {
     id: 'u2',
     nameFirst: 'Ya ',
     nameLast: 'Dad',
     email: 'ya_dad@yafamily.com',
-    userMastered: [],
-    userNotMastered: [],
+    userMastered: <Technique[]>[],
+    userNotMastered: <Technique[]>[],
   },
   {
     id: 'u3',
     nameFirst: 'Ya ',
     nameLast: 'Dog',
     email: 'ya_dog@yafamily.com',
-    userMastered: [],
-    userNotMastered: [],
+    userMastered: <Technique[]>[],
+    userNotMastered: <Technique[]>[],
   },
 ]
