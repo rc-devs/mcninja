f6e4c03555e1e8ebe496ad341f03440e0e019d3d
rc-devs <rc.devs9@gmail.com>
2025-04-07 13:57:21 -0500

    exercise dummy data

---
+++ mcninja/src/app/data/exercises.data.ts
@@ -0,0 +1,28 @@
+import { Exercise } from "./models/exercise.model";
+
+export default<Exercise[]>[
+  {
+    id: 'e1',
+    form: 'Burpee',
+    iteration: 10,
+    duration: 30,
+  },
+  {
+    id: 'e2',
+    form: 'Air Squat',
+    iteration: 10,
+    duration: 20,
+  },
+  {
+    id: 'e3',
+    form: 'Push-up',
+    iteration: 15,
+    duration: 40,
+  },
+  {
+    id: 'e4',
+    form: 'Plank',
+    iteration: 10,
+    duration: 30,
+  },
+]
