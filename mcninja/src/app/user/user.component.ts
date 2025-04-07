025a0b298f525f9bde58fb793988de5182b7bb30
rc-devs <rc.devs9@gmail.com>
2025-04-07 15:04:11 -0500

    remove test

--- mcninja/src/app/user/user.component.ts
+++ mcninja/src/app/user/user.component.ts
@@ -1,19 +1,14 @@
 import { Component, Input, signal } from '@angular/core';
 import { User } from '../data/models/user.model';
 import userData from '../data/user.data';
-import { ExercisesComponent } from '../exercises/exercises.component';
 
 @Component({
   selector: 'app-user',
-  imports: [ExercisesComponent],
+  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
  users = signal<User[]>(userData)
 
- onClick(exercises:any ){
-  console.log(exercises)
-  console.log(typeof exercises)
- }
 }
