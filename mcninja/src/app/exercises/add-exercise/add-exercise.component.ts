95597df7cd08014ff41c9438676e4b6f1abf898b
rc-devs <rc.devs9@gmail.com>
2025-04-08 13:35:33 -0500

    create new id dynamically!

--- mcninja/src/app/exercises/add-exercise/add-exercise.component.ts
+++ mcninja/src/app/exercises/add-exercise/add-exercise.component.ts
@@ -11,13 +11,18 @@ import { ExerciseService } from '../../shared/exercise-service/exercise-service.
 export class AddExerciseComponent {
    exercisesService = inject(ExerciseService);
 
+
   addExerciseHandler(name:string, iteration:any, duration:any, form:any){
+    //get array length for new id
+    let arrayLength = this.exercisesService.exercisesArray.length + 1 //array length +1 bc it will be pushed to array afterwards
     //add new object to array
    this.exercisesService.exercisesArray.push({
-    id: 'test',
+    id: 'e' + arrayLength, //create new id dynamically to avoid NG0955 error
     form: name,
     iteration: iteration,
     duration: duration,})
+
+
 
     console.log(this.exercisesService.exercisesArray) //test log
 
