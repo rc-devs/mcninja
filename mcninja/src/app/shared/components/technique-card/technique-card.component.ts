4bdf4217e6771de8f2de21ae275f6e8408ebaeae
rc-devs <rc.devs9@gmail.com>
2025-05-09 08:11:43 -0500

    gc via cli

---
+++ mcninja/src/app/shared/components/technique-card/technique-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technique-card',
+  imports: [],
+  templateUrl: './technique-card.component.html',
+  styleUrl: './technique-card.component.css'
+})
+export class TechniqueCardComponent {
+
+}
