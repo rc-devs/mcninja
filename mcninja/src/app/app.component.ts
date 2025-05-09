ccfc484175bc8751f9023428ce5d68cec35871a9
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:42:00 -0500

    router.vavigate onInit

--- mcninja/src/app/app.component.ts
+++ mcninja/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApplayoutComponent } from './features/applayout/applayout.component';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TechniqueCardComponent } from './shared/components/technique-card/technique-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ApplayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mcninja';
-}
+ constructor(private router: Router){};
+
+ ngOnInit(): void {
+  this.router.navigate([''])
+  }
+};
