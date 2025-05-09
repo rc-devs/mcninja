da8324fffe59a115eefb199c4b0de3b9bd2855ff
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:41:16 -0500

    routes app-layout, technque card (test)

--- mcninja/src/app/app.routes.ts
+++ mcninja/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//imported route components
+import { TechniqueCardComponent } from './shared/components/technique-card/technique-card.component';
+import { ApplayoutComponent } from './features/applayout/applayout.component';
+
+export const routes: Routes = [
+
+  {path: '', redirectTo: 'applayout', pathMatch: 'full'},
+  {
+    path: 'applayout',
+    component: ApplayoutComponent,
+    children: [
+      {
+        path: 'technique-card',
+        component: TechniqueCardComponent,
+      },
+    ],
+  },
+];
