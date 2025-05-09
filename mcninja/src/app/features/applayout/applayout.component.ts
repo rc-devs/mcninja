4a27684d5d84b02dccf50f5d407f28a7090ac18c
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:41:47 -0500

    import techniqueCard

--- mcninja/src/app/features/applayout/applayout.component.ts
+++ mcninja/src/app/features/applayout/applayout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TechniqueCardComponent } from '../../shared/components/technique-card/technique-card.component';
 
 @Component({
   selector: 'app-applayout',
-  imports: [RouterOutlet],
+  imports: [TechniqueCardComponent],
   templateUrl: './applayout.component.html',
   styleUrl: './applayout.component.css'
 })
