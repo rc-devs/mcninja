8c10f723abd73a2841a481720509d46bf2db01dc
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:04:13 -0500

    router outlet

--- mcninja/src/app/features/applayout/applayout.component.ts
+++ mcninja/src/app/features/applayout/applayout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-applayout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './applayout.component.html',
   styleUrl: './applayout.component.css'
 })
