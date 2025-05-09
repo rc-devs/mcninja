335e64b9e0aa0f456cc78e1130bc507f850dc29f
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:05:40 -0500

    app-layout

--- mcninja/src/app/app.component.ts
+++ mcninja/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ApplayoutComponent } from './features/applayout/applayout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ApplayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
