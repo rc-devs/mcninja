f05a49c6c5a3264d5445671fb356a9e21a82714f
rc-devs <rc.devs9@gmail.com>
2025-05-09 09:03:20 -0500

    gc app layout

---
+++ mcninja/src/app/features/applayout/applayout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applayout',
+  imports: [],
+  templateUrl: './applayout.component.html',
+  styleUrl: './applayout.component.css'
+})
+export class ApplayoutComponent {
+
+}
