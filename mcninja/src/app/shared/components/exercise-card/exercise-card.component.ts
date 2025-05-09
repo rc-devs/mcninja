9fa4676bd5bfd55f3e556938059debf1ebfe0a21
rc-devs <rc.devs9@gmail.com>
2025-05-09 08:09:31 -0500

    new component via cli

---
+++ mcninja/src/app/shared/components/exercise-card/exercise-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-card',
+  imports: [],
+  templateUrl: './exercise-card.component.html',
+  styleUrl: './exercise-card.component.css'
+})
+export class ExerciseCardComponent {
+
+}
